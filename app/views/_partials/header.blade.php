7036411b437191e1bb33709245b1e0799dbecdc3
kennyalmendral <kennyalmendral@gmail.com>
2015-01-12 12:31:37 +0800

    Modify header.

--- app/views/_partials/header.blade.php
+++ app/views/_partials/header.blade.php
@@ -38,69 +38,70 @@ $game_settings = GameSetting::all();
 
 	@include('_partials/side_menu')
 
-	<div id="container">
+	<div id="header">	
 
-		<div id="header">	
+		<div class="container clearfix">
+			<a href="#" id="nav-toggle" class="menu-btn"><i class="fa fa-bars"></i></a>
+			<a href="{{ URL::previous(); }}" id="back"><i class="fa fa-angle-left"></i></a>
 
-			<div class="container clearfix">
-				<a href="#" id="nav-toggle" class="menu-btn"><i class="fa fa-bars"></i></a>
-				<a href="{{ URL::previous(); }}" id="back"><i class="fa fa-angle-left"></i></a>
+			<?php $tdrive = $general_settings[0]->value ?>
 
-				<?php $tdrive = $general_settings[0]->value ?>
+			<a href="{{ route('home.show') }}" id="tdrive">{{ HTML::image("images/tdrive.png", "$tdrive", array('class' => 'auto')) }}</a>
 
-				<a href="{{ route('home.show') }}" id="tdrive">{{ HTML::image("images/tdrive.png", "$tdrive", array('class' => 'auto')) }}</a>
+			<div class="tablet fl clearfix">
+				<ul class="menu fl">
+					<li><a href="{{ route('home.show') }}#latest-games" class="menu-games">{{ trans('global.games') }}</a></li>
+					<li><a href="{{ route('home.show') }}#news" class="menu-news">{{ trans('global.news') }}</a></li>
+					<li><a href="{{ route('home.show') }}#faqs" class="menu-faqs">{{ trans('global.faqs') }}</a></li>
+					<li><a href="{{ route('home.show') }}#contact" class="menu-contact">{{ trans('global.contact') }}</a></li>
+				</ul>
 
-				<div class="tablet fl clearfix">
-					<ul class="menu fl">
-						<li><a href="{{ route('home.show') }}#latest-games" class="menu-games">{{ trans('global.games') }}</a></li>
-						<li><a href="{{ route('home.show') }}#news" class="menu-news">{{ trans('global.news') }}</a></li>
-						<li><a href="{{ route('home.show') }}#faqs" class="menu-faqs">{{ trans('global.faqs') }}</a></li>
-						<li><a href="{{ route('home.show') }}#contact" class="menu-contact">{{ trans('global.contact') }}</a></li>
-					</ul>
+				<div class="fl">
+					<div class="top">
 
-					<div class="fl">
-						<div class="top">
+						@if (Auth::check())
+							
+							<p><a href="{{ route('user.profile', Auth::user()->id) }}"><i class="fa fa-user"></i> {{ Auth::user()->username }}</a> | {{ link_to_route('users.logout', trans('global.logout')) }}</p>
 
-							@if (Auth::check())
-								
-								<p><a href="{{ route('user.profile', Auth::user()->id) }}"><i class="fa fa-user"></i> {{ Auth::user()->username }}</a> | {{ link_to_route('users.logout', trans('global.logout')) }}</p>
+						@else
 
-							@else
+							<ul>
+								<li><a href="{{ route('users.login') }}" class="login">{{ trans('global.login') }}</a></li>
+								<li><a href="{{ route('users.register') }}" class="register">{{ trans('global.register') }} <i class="fa fa-user"></i></a></li>
+							</ul>
 
-								<ul>
-									<li><a href="{{ route('users.login') }}" class="login">{{ trans('global.login') }}</a></li>
-									<li><a href="{{ route('users.register') }}" class="register">{{ trans('global.register') }} <i class="fa fa-user"></i></a></li>
-								</ul>
-
-							@endif
-
-						</div>
-
-						<div id="polyglotLanguageSwitcher1" class="polyglotLanguageSwitcher">
-							<form action="{{ URL::route('choose_language') }}" id="locale" class="language" method="post">
-
-								{{ Form::token() }}
-
-								<select name="locale" id="polyglot-language-options">
-									<option id="en" value="en" {{ (strtolower($user_location['isoCode']) == 'us' || Lang::locale() == 'us') ? ' selected' : '' }}>English</option>
-									<option id="th" value="th" {{ (strtolower($user_location['isoCode']) == 'th' || Lang::locale() == 'th') ? ' selected' : '' }}>Thai</option>
-									<option id="id" value="id" {{ (strtolower($user_location['isoCode']) == 'id' || Lang::locale() == 'id') ? ' selected' : '' }}>Bahasa Indonesia</option>
-									<option id="my" value="my" {{ (strtolower($user_location['isoCode']) == 'my' || Lang::locale() == 'my') ? ' selected' : '' }}>Bahasa Malaysia</option>
-									<option id="cn" value="cn" {{ (strtolower($user_location['isoCode']) == 'cn' || Lang::locale() == 'cn') ? ' selected' : '' }}>Traditional Chinese</option>
-									<option id="cn" value="cn" {{ (strtolower($user_location['isoCode']) == 'cn' || Lang::locale() == 'cn') ? ' selected' : '' }}>Simplified Chinese</option>
-									<option id="vn" value="vn" {{ (strtolower($user_location['isoCode']) == 'vn' || Lang::locale() == 'vn') ? ' selected' : '' }}>Vietnamese</option>
-									<option id="jp" value="jp" {{ (strtolower($user_location['isoCode']) == 'jp' || Lang::locale() == 'jp') ? ' selected' : '' }}>Japanese</option>
-									<option id="hi" value="hi" {{ (strtolower($user_location['isoCode']) == 'hi' || Lang::locale() == 'hi') ? ' selected' : '' }}>Hindi</option>
-								</select>
-
-								<input type="submit" value="select">
-							</form>
-						</div>
+						@endif
 
 					</div>
-				</div>
 
-				<a href="http://tose.com.ph" id="tose" target="_blank">{{ HTML::image('images/tose.png', 'TOSE') }}</a>
+					<div id="polyglotLanguageSwitcher1" class="polyglotLanguageSwitcher">
+						<form action="{{ URL::route('choose_language') }}" id="locale" class="language" method="post">
+
+							{{ Form::token() }}
+
+							<select name="locale" id="polyglot-language-options">
+								<option id="en" value="en" {{ (strtolower($user_location['isoCode']) == 'us' || Lang::locale() == 'us') ? ' selected' : '' }}>English</option>
+								<option id="th" value="th" {{ (strtolower($user_location['isoCode']) == 'th' || Lang::locale() == 'th') ? ' selected' : '' }}>Thai</option>
+								<option id="id" value="id" {{ (strtolower($user_location['isoCode']) == 'id' || Lang::locale() == 'id') ? ' selected' : '' }}>Bahasa Indonesia</option>
+								<option id="my" value="my" {{ (strtolower($user_location['isoCode']) == 'my' || Lang::locale() == 'my') ? ' selected' : '' }}>Bahasa Malaysia</option>
+								<option id="cn" value="cn" {{ (strtolower($user_location['isoCode']) == 'cn' || Lang::locale() == 'cn') ? ' selected' : '' }}>Traditional Chinese</option>
+								<option id="cn" value="cn" {{ (strtolower($user_location['isoCode']) == 'cn' || Lang::locale() == 'cn') ? ' selected' : '' }}>Simplified Chinese</option>
+								<option id="vn" value="vn" {{ (strtolower($user_location['isoCode']) == 'vn' || Lang::locale() == 'vn') ? ' selected' : '' }}>Vietnamese</option>
+								<option id="jp" value="jp" {{ (strtolower($user_location['isoCode']) == 'jp' || Lang::locale() == 'jp') ? ' selected' : '' }}>Japanese</option>
+								<option id="hi" value="hi" {{ (strtolower($user_location['isoCode']) == 'hi' || Lang::locale() == 'hi') ? ' selected' : '' }}>Hindi</option>
+							</select>
+
+							<input type="submit" value="select">
+						</form>
+					</div>
+
+				</div>
 			</div>
 
-		</div><!-- end #header -->
+			<a href="http://tose.com.ph" id="tose" target="_blank">{{ HTML::image('images/tose.png', 'TOSE') }}</a>
+		</div>
+
+	</div><!-- end #header -->
+
+	<div id="container">
+
