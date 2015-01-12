0403676afde5f5acd7dad23fe31bd5763213877d
geneconde <s.conde@leadhope.com>
2015-01-12 13:05:28 +0800

    Sharing and Like Buttons

--- app/views/news.blade.php
+++ app/views/news.blade.php
@@ -1,5 +1,9 @@
 @extends('_layouts/single')
 
+@section('stylesheets')
+	{{ HTML::style("css/jquery.fancybox.css"); }}
+@stop
+
 @section('content')
 
 	<div id="featured" class="container">
@@ -31,8 +35,21 @@
 
 		<div class="social clearfix">
 			<div>
-				<a href="#" class="share">Share</a>
-				<a href="#" class="like">Like </a>
+				<a href="#share" id="inline" class="share">Share</a>
+				<div href="#" class="like">Like 
+					<div id="news_like" class="fb-like" data-href="{{ url() }}/game/{{ $news->id }}" data-width="150px" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
+				</div>
+			</div>
+			<div style="display:none">
+				<div id="share" style="text-align:center;">
+					<h4 style="margin: 10px 0;">Share the game to the following social networks.</h4>
+					<a style="margin:0 2px;" href="http://www.facebook.com/sharer/sharer.php?s=100&amp;p[url]={{ url() }}/news/{{ $news->id }}" data-social='{"type":"facebook", "url":"{{ url() }}/news/{{ $news->id }}", "text": "{{ $news->main_title }}"}'>
+						{{ HTML::image('images/icon-social-facebook.png', 'Share', array('class' => 'auto')) }}
+					</a>
+					<a style="margin:0 2px;" href="https://twitter.com/share?url={{ url() }}/news/{{ $news->id }}" data-social='{"type":"twitter", "url":"{{ url() }}/news/{{ $news->id }}", "text": "{{ $news->main_title }}"}'>
+						{{ HTML::image('images/icon-social-twitter.png', 'Share', array('class' => 'auto')) }}
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -41,8 +58,20 @@
 
 @section('javascripts')
 	{{ HTML::script("js/fastclick.js"); }}
+	{{ HTML::script("js/jquery.fancybox.js"); }}
+	{{ HTML::script("js/jqSocialSharer.min.js"); }}
 
 	<script>
 		FastClick.attach(document.body);
+
+		$("#inline").fancybox({
+            'titlePosition'     : 'inside',
+            'transitionIn'      : 'none',
+            'transitionOut'     : 'none'
+        });
+
+        $("#share a").jqSocialSharer();
+
+        $('.fancybox').fancybox({ padding: 0 });
 	</script>
 @stop
