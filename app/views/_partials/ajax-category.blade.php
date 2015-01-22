2237e09354396e14ec226b00dc98119fd8899843
jmrlipio <jmrlipio@gmail.com>
2015-01-22 19:19:31 +0800

    Added ribbon to _partials.ajax and search blade

--- app/views/_partials/ajax-category.blade.php
+++ app/views/_partials/ajax-category.blade.php
@@ -6,10 +6,13 @@
 			<div class="item">
 				<div class="thumb relative">
 					@if ($game->default_price == 0)
-						<a href="{{ URL::route('game.show', $game->id) }}">{{ HTML::image('images/ribbon.png', 'Free', array('class' => 'free auto')) }}</a>
+						<a href="{{ URL::route('game.show', $game->id) }}">{{ HTML::image('images/ribbon-back.png', 'Free', array('class' => 'free-back auto')) }}</a>
 					@endif
 
-					<a href="{{ URL::route('game.show', $game->id) }}">{{ HTML::image('assets/games/icons/' . $media->url, $game->main_title) }}</a>
+					<a href="{{ URL::route('game.show', $game->id) }}" class="thumb-image">{{ HTML::image('assets/games/icons/' . $media->url, $game->main_title) }}</a>
+					@if ($game->default_price == 0)
+						<a href="{{ URL::route('game.show', $game->id) }}">{{ HTML::image('images/ribbon-front.png', 'Free', array('class' => 'free-front auto')) }}</a>
+					@endif
 				</div>
 
 				<div class="meta">
