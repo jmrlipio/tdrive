96647dcbfddf92074df7301e7aef85278629d336
Marvin <mjbaga@yahoo.com>
2015-03-06 19:08:56 +0800

    did minor css edits on thumbnails

--- app/views/related.blade.php
+++ app/views/related.blade.php
@@ -24,7 +24,6 @@
 											<a href="{{ URL::route('game.show', $game->id) }}">{{ HTML::image('images/ribbon.png', 'Free', array('class' => 'free auto')) }}</a>
 										@endif
 
-										<img src="{{ Request::root() . '/assets/games/icons/'. $media->url }}" alt="{{ $game->main_title }}">
 							<!-- If image url can't be located, change the url to -->
 							<!--  {{ Request::root() . '/assets/games/icons/'. $media->url }}" alt="{{ $game->main_title }}  -->
 										<a href="{{ URL::route('game.show', $game->id) }}">{{ HTML::image('assets/games/icons/' . $media->url, $game->main_title) }}</a>
