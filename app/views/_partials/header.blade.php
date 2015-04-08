e21a415a433c4b7bcd1375e21a8566125ab161ab
jmrlipio <jmrlipio@gmail.com>
2015-04-08 16:33:02 +0800

    Header update

--- app/views/_partials/header.blade.php
+++ app/views/_partials/header.blade.php
@@ -214,12 +214,10 @@ $game_settings = GameSetting::all();
 							$languages = Language::whereIn('id', $lang_id)->get();
 
 						?>
-						{{ Session::get('locale') }}
+						{{-- Session::get('locale') --}}
 						<form action="{{ URL::route('choose_language') }}" id="locale" class="language" method="post">
 
 							<select name="locale" id="polyglot-language-options">
-
-								
 								
 								@foreach($languages as $lang)												
 									<?php $selected = (strtolower($lang->iso_code) == Session::get('locale')) ? ' selected' : ''; ?>
