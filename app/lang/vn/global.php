3b3df7d15a712e18db119c0e714424969fed89e3
jcaluminga <jcaluminga@gmail.com>
2015-04-10 16:01:11 +0800

    Updates Text Translations for Viet and Indo Language

---
+++ app/lang/vn/global.php
@@ -0,0 +1,152 @@
+<?php
+
+return array(
+	//Global
+	'games' => 'trò chơi',
+	'news' => 'tin tức',
+	'faqs' => 'faqs',
+	'contact' => 'tiếp xúc',
+	'login' => 'đăng nhập',
+	'register' => 'đăng ký',
+	'logout' => 'đăng xuất',
+	'test' => ':thử',
+	'Select Carrier' => 'Chọn nhà cung cấp',
+	'loading' => 'tải',
+	'name' => 'tên',
+	'email' => 'email',
+	'message' => 'thông điệp',
+	'username' => 'tên người dùng',
+	'password' => 'mật khẩu',
+	'Sign In' => 'Đăng Nhập',
+	'submit' => 'đệ trình',
+	'All games' => 'Tất cả trò chơi',
+	'All Categories' => 'Tất cả danh mục',
+	'your email/password was incorrect' => 'email / mật khẩu không chính xác',
+	'Please select' => 'Vui lòng chọn',
+	'search game' => 'trò chơi tìm kiếm',
+	'View all categories' => 'Xem tất cả các hạng mục',
+	'first_name' => 'tên',
+	'last_name' => 'last_name',
+	'mobile_no' => 'không có điện thoại di động',
+	'password_confirmation' => 'password_confirmation',
+
+	//Categories
+	'Brain and Puzzle' => 'Não và Puzzle',
+	'Casual' => 'tình cờ',
+	'Arcade' => 'Arcade',
+	'Cards and Casino' => 'Thẻ và Casino',
+	'Classic' => 'cổ điển',
+	'Sanrio' => 'Sanrio',
+
+	// Error and Success Messages
+	'Login successful' => 'Đăng nhập thành công',
+	'Incorrect username or password' => 'Tên đăng nhập hoặc mật khẩu không chính xác',
+	'Review successfully submitted' => 'Xem lại gửi thành công',
+	'One or more fields are incomplete' => 'Một hoặc nhiều trường chưa đầy đủ',
+	'Your message has successfully been submitted' => 'Thông điệp của bạn đã thành công được nộp',
+	'One or more fields are incomplete' => 'Một hoặc nhiều trường chưa đầy đủ',
+	'You have successfully registered' => 'Bạn đã đăng ký thành công',
+	'One or more fields are incomplete' => 'Một hoặc nhiều trường chưa đầy đủ',
+	'Password request has been submitted' => 'Mật khẩu yêu cầu đã được gửi',
+	'Email not found' => 'Email không tìm thấy',
+
+	//404 Error Blade (404.blade.php)
+	'Page Not Found!' => 'Không tìm thấy trang!',
+	'This page could not be found on the server.' => 'Trang này không thể được tìm thấy trên máy chủ.',
+	'404 error!' => '404 lỗi!',
+	'Return to homepage' => 'Trở về trang chủ',
+
+	//Category Blade (category.blade.php)
+	'search games in this category' => 'trò chơi tìm kiếm trong mục này',
+
+	//Category Listing Blade (category_listing.blade.php)
+	'Category list' => 'danh mục thể loại',
+	'Categories not found.' => 'Categories not found.',
+
+	//Game Blade (game.blade.php)
+	'Downloads' => 'Downloads',
+	'Buy Now' => 'mua Ngay',
+	'Share' => 'phần',
+	'Share the game to the following social networks.' => 'Chia sẻ các trò chơi với các mạng xã hội sau.',
+	'Rating' => 'Đánh giá',
+	'Login to write a review' => 'Đăng nhập để viết bình luận',
+	'See all reviews' => 'Xem tất cả các đánh giá',
+	'Related games' => 'trò chơi liên quan',
+	'More' => 'hơn',
+	'total' => 'tổng số',
+	'Release' => 'phóng thích',
+
+	//Games Blade (games.blade.php)
+	'new and updated games' => 'các trò chơi mới và cập nhật',
+	
+	//Index Blade (index.blade.php)
+	'Games' => 'trò chơi',
+	'See all' => 'xem tất cả',
+	'latest news' => 'tin mới nhất',
+	'Read more' => 'đọc thêm',
+	'FAQs' => 'Câu hỏi thường gặp',
+	'Find answers to Frequently Asked Questions about TDrive and our services below.' => 'Tìm câu trả lời cho câu hỏi thường gặp về ổ và dịch vụ của chúng tôi dưới đây.',
+	'Contact us' => 'liên hệ với chúng tôi',
+	'Your comments and suggestions are important to us. You can reach us via the contact points below.' => 'Ý kiến và đề xuất của bạn rất quan trọng đối với chúng tôi. Bạn có thể đến với chúng ta qua các điểm tiếp xúc bên dưới.',
+	'General Inquiry' => 'Tin nhắn của bạn nói chung',
+	'View' => 'quang cảnh',
+	'type what you see...' => 'gõ những gì bạn nhìn thấy ...',
+	
+	//Login Blade (login.blade.php)
+	'Forgot your password?' => 'Quên mật khẩu?',
+	'Not yet a member?' => 'Chưa là thành viên?',
+	'Register' => 'Đăng ký',
+
+	//News Listing Blade (news_listing.blade.php)
+	'Latest news' => 'tin mới nhất',
+
+	//Payment Blade (payment.blade.php)
+	'Payment Information' => 'Thông tin thanh toán',
+
+	//Profile Blade (profile.blade.php)
+	'Change Password' => 'đổi Mật Khẩu',
+	'Downloaded games' => 'trò chơi tải về',
+	'Buy' => 'mua',
+
+	//Register Blade (register.blade.php)
+	'Create new account' => 'Tạo tài khoản mới',
+	'password_confirmation' => 'xác nhận mật khẩu',
+
+	//Related Blade (related.blade.php)
+	'Related games' => 'trò chơi liên quan',
+
+	//Reviews Blade (reviews.blade.php)
+	'All reviews for' => 'Tất cả các ý kiến cho',
+
+	//Search Blade (search template.php)
+	'Search results' => 'kết quả tìm kiếm',
+	'Sorry, there are no results for' => 'Xin lỗi, không có kết quả cho',
+
+	//Footer (_partials/footer.blade.php)
+	'Copyright' => 'bản quyền',
+	'All rights reserved' => 'Tất cả các quyền',
+
+	//Desktop (desktop/index.blade.php)
+	'Ooops, Desktop view is not available' => 'Rất tiếc, view Desktop không có sẵn',
+	'Please view this on mobile or tablet' => 'Xin vui lòng xem trên điện thoại di động hoặc máy tính bảng',
+
+	//Emails (auth/activate.blade.php)
+	'Hello' => 'Xin chào',
+	'Please activate your account using the following link' => 'Vui lòng kích hoạt tài khoản của bạn bằng cách sử dụng liên kết sau đây',
+
+	//Emails (auth/reminder.blade.php)
+	'Password Reset' => 'Đặt Lại Mật Khẩu',
+	'To reset your password, complete this form:' => 'Để thiết lập lại mật khẩu của bạn, hoàn thành mẫu này:',
+	'This link will expire in' => 'Liên kết này sẽ hết hạn trong',
+	'minutes' => 'từ phút',
+
+	//Emails (inquiries/inquire.blade.php and replyto.blade.php)
+	'Inquiry' => 'Tin nhắn của bạn',
+
+	//Password (remind.blade.php)
+	'reset password' => 'đặt lại mật khẩu',
+
+	//Password (reset.blade.php)
+	'Please fill up this form to reset your password' => 'Hãy điền vào mẫu này để thiết lập lại mật khẩu của bạn',
+
+);
