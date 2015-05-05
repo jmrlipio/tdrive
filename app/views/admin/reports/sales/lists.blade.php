aafbc8162d63309dea535ab61566accaa5986344
jmrlipio <jmrlipio@gmail.com>
2015-05-05 15:58:25 +0800

    Added clear both in tables in reports page.

--- app/views/admin/reports/sales/lists.blade.php
+++ app/views/admin/reports/sales/lists.blade.php
@@ -15,17 +15,18 @@
 			</tr>
 			<thead>
 			<tbody>
-			@foreach($sales as $sale)
-				<tr>
-					<td style="width: 300px;">{{ $sale->prices->game->main_title }}</td>
-					<td style="width: 80px;" >{{ $sale->prices->carrier->carrier }}</td>
-					<td style="width: 280px;">{{ $sale->prices->country->capital }}</td>
-					<td style="width: 180px;">{{ $sale->user->username }}</td>
-					<td style="width: 180px;">{{ $sale->prices->price }}</td>
-				</tr>
-			@endforeach
-		</tbody>
+				@foreach($sales as $sale)
+					<tr>
+						<td style="width: 300px;">{{ $sale->prices->game->main_title }}</td>
+						<td style="width: 80px;" >{{ $sale->prices->carrier->carrier }}</td>
+						<td style="width: 280px;">{{ $sale->prices->country->capital }}</td>
+						<td style="width: 180px;">{{ $sale->user->username }}</td>
+						<td style="width: 180px;">{{ $sale->prices->price }}</td>
+					</tr>
+				@endforeach
+			</tbody>
 		</table>
+		<div class="clear"></div>
 	</div>
 	{{ HTML::script('js/form-functions.js') }}
 	{{ HTML::script('js/jquery.dataTables.js') }}
