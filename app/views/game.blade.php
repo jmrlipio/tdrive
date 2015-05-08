5c7586ae5384a0f45434957aa069e2150cd9cfb2
jmrlipio <jmrlipio@gmail.com>
2015-05-08 19:28:36 +0800

    Fixed price on buy btn

--- app/views/game.blade.php
+++ app/views/game.blade.php
@@ -117,7 +117,7 @@
 
 								@unless ($apps->pivot->price == 0)
             
-					            	<?php $dc = GameDiscount::checkDiscountedGames($game->id, $discounted_games);
+					            	<?php $dc = Discount::checkDiscountedGame($game->id);
 					              		$sale_price = $apps->pivot->price * (1 - ($dc/100));
 					              	?>
 					             
