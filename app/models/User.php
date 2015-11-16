afdf80ee61566d12d964681f3cc7ce0b30a39621
jmrlipio <jmrlipio@gmail.com>
2015-11-16 15:43:14 +0800

    #244 - IP Filter

--- app/models/User.php
+++ app/models/User.php
@@ -97,6 +97,11 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
         return $this->belongsToMany('GamePrice', 'game_sales');
     }
 
+    public function ipfilters() {
+
+        return $this->hasMany('IPFilter');
+    }
+
     public static function updateLastLogin($id) 
     {
         $user = User::find($id);
